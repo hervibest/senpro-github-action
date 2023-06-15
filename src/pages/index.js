7e4c3a246aa8d9b1db27cc528f7bc7ac2d0c80bb
Hervi Nur Rahmandien <71449881+hervibest@users.noreply.github.com>
2023-06-15 17:37:37 +0700

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Lakukan perubahan pada&nbsp;
+            Lakukan perubahan padas&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
         </div>
